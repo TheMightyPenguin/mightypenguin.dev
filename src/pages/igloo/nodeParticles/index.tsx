61eb43f3b7bb81d2e61c84a30a19e881de0207d7
Victor Tortolero <victor.tortolero@medwing.com>
2021-01-20 17:18:33 -0600

    Render black background on Node Particles

---
+++ src/pages/igloo/nodeParticles/index.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from "react";
+
+const renderAnimation = (canvas: HTMLCanvasElement) => {
+  let keepRunning = true;
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  const ctx = canvas.getContext('2d');
+
+  if (!ctx) {
+    return () => {};
+  }
+
+  const update = () => {
+    ctx.fillStyle = 'black';
+
+    ctx.fillRect(0, 0, window.innerHeight, window.innerHeight);
+
+    if (keepRunning) {
+      window.requestAnimationFrame(update);
+    }
+  };
+
+  window.requestAnimationFrame(update);
+
+  return () => {
+    keepRunning = false;
+  }
+};
+
+const Particles = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const unsubscribe = renderAnimation(canvasRef.current);
+
+    return () => {
+      unsubscribe();
+    }
+  }, [canvasRef]);
+
+  return <div>
+    <canvas ref={canvasRef} />
+  </div>;
+};
+
+export default Particles;
--- src/pages/igloo/particles.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Particles = () => {
-  return <div>Particles</div>;
-};
-
-export default Particles;
