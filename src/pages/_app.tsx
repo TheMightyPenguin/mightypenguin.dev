6cef3e62dcad5a4885dbfba8ca080868a518c756
Victor Tortolero <victor.tortolero@medwing.com>
2021-01-23 22:45:10 -0600

    Remove ; causing overflow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
           rel="stylesheet"
         />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 };
