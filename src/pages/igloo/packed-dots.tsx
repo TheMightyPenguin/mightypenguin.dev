a3cbcec575e4f8d30b68895c830347a474ad5543
Victor Tortolero <victormtortolero@gmail.com>
2022-08-06 16:17:22 -0500

    Improve packed dots colors

--- src/pages/igloo/packed-dots.tsx
+++ src/pages/igloo/packed-dots.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useReducer } from 'react';
+import colorPalettes from 'nice-color-palettes';
+import React, { useMemo, useReducer } from 'react';
 
 import BackToLink from '@/components/BackToLink/BackToLink';
 import PackedDots from '@/components/canvas/PackedDots/PackedDots';
@@ -9,10 +10,14 @@ const PackedDotsPage = () => {
   useIgnoreScroll();
   const [count, increment] = useReducer((c) => c + 1, 0);
 
-  const colors = {
-    background: '#fff2f1',
-    others: ['#5F00BA', '#A09BE7', '#C03221', '#231F20'],
-  };
+  const colors = useMemo(() => {
+    const palette =
+      colorPalettes[Math.floor(Math.random() * colorPalettes.length)];
+    return {
+      background: palette[0],
+      others: palette.slice(1),
+    };
+  }, [count]);
 
   return (
     <React.Fragment>
